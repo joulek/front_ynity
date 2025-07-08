4dfee89496b675436811ef70922ad1ca1dddaf4f
ynitylearn <ynitylearn@gmail.com>
2025-07-08 14:21:16 +0100

    first commit87

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -5,16 +5,17 @@ export const UserContext = createContext();
 export function UserProvider({ children }) {
   const [user, setUser] = useState(null);
   const [loadingUser, setLoadingUser] = useState(true);
-
   useEffect(() => {
     fetch(`${import.meta.env.VITE_BACKEND_URL}/auth/user`, {
-      credentials: "include",
+      method: "GET",               // ✅ Obligatoire
+      credentials: "include",      // ✅ Pour envoyer les cookies
     })
       .then((res) => (res.ok ? res.json() : null))
       .then((data) => setUser(data))
       .catch(() => setUser(null))
       .finally(() => setLoadingUser(false));
   }, []);
+
 
   return (
     <UserContext.Provider value={{ user, loadingUser }}>
